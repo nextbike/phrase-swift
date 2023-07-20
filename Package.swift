26cdc04bc23cccc3bd0a3c508dee9e73c0bbceb0
Jonas Vogel <jonas.vogel@tier.app>
2023-07-20 10:42:13 +0200

    Fix package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "phrase-swift",
+    name: "PhraseSwift",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
